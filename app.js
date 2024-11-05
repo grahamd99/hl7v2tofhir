117a6a66c6da16f7282c2a414af56eaac51f0a84
grahamd99 <graham.dodsworth@nhs.net>
2024-11-05 08:06:55 +0000

    Move input FHIR JSON to separate file

--- app.js
+++ app.js
@@ -1,34 +1,26 @@
 // app.js
 const { loadMapping, convertToHL7v2 } = require('./convert');
-
-// Sample FHIR ServiceRequest JSON
-const serviceRequest = {
-  "resourceType": "ServiceRequest",
-  "id": "mammography-request-123",
-  "status": "active",
-  "intent": "order",
-  "code": {
-    "coding": [
-      {
-        "system": "http://snomed.info/sct",
-        "code": "24623002",
-        "display": "Screening mammography"
-      }
-    ],
-    "text": "Screening mammography"
-  },
-  "subject": {
-    "reference": "Patient/123"
-  }
-};
+const path = require('path')
+const fs = require('fs')
 
 // Main function to perform conversion
 const main = () => {
+const filePath = path.resolve(__dirname, './public/FhirServiceRequestMammo.json');
   const mapping = loadMapping();
-  const hl7Message = convertToHL7v2(serviceRequest, mapping);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const serviceRequest = JSON.parse(data);
+    //console.log(data);
+
+    const hl7Message = convertToHL7v2(serviceRequest, mapping);
   
-  console.log("Generated HL7 ORM Message:");
-  console.log(hl7Message);
+    console.log("Generated HL7 ORM Message:");
+    console.log(hl7Message);
+  })
 };
 
 main();
