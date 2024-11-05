957bac56745fc9d82c5ccdb960a6bee3e9859943
grahamd99 <graham.dodsworth@nhs.net>
2024-11-05 19:59:01 +0000

    Add prettier and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # hl7v2tofhir
+
+# Prettier
+
+This repo uses https://prettier.io/  
+To format all files with Prettier
+
+```
+npx prettier . --write
+```
--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // app.js
-const { loadMapping, convertToHL7v2 } = require('./convert');
-const path = require('path')
-const fs = require('fs')
+const { loadMapping, convertToHL7v2 } = require("./convert");
+const path = require("path");
+const fs = require("fs");
 
 // Main function to perform conversion
 const main = () => {
-const filePath = path.resolve(__dirname, './public/FhirServiceRequestMammo.json');
+  const filePath = path.resolve(
+    __dirname,
+    "./public/FhirServiceRequestMammo.json"
+  );
   const mapping = loadMapping();
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
@@ -17,10 +20,10 @@ const filePath = path.resolve(__dirname, './public/FhirServiceRequestMammo.json'
     //console.log(data);
 
     const hl7Message = convertToHL7v2(serviceRequest, mapping);
-  
+
     console.log("Generated HL7 ORM Message:");
     console.log(hl7Message);
-  })
+  });
 };
 
 main();
